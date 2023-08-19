49133e9c7ae039b54354e7eecfb286c8f45894e5
Sagnik Pal <palsagnik2001@gmail.com>
2023-08-19 16:39:27 +0530

    Loader Issue Resolved

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       upadateLoad(false);
-    }, 1200);
+    }, 5000);
 
     return () => clearTimeout(timer);
   }, []);
